9c2379a117f4ee6206895a16b9f6b0763c3d05b6
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2021-09-13 14:29:10 -0600

    adding error handling functions, getting ready for heroku deploy..

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 
 const mock_data = require("./mock_data.json")
 const fileSystem = require('fs')
 const path = require('path')
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.get('/history', (req, res) => {
     setTimeout(() => {
         res.send(mock_data)        
@@ -17,6 +19,15 @@ app.get('/recording', (req, res) => {
   opStream.pipe(res)
 })
 
+app.use((err, res, next) => {
+  res.status(404).send({error: 'Not found!', status: 404})
+})
+app.use((err, res, next) => {
+  console.error('Error Handler:', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack})
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(process.env.NODE_ENV !== 'production' ? `Example app listening at http://localhost:${port}` : `https://HEROKU_URL_HERE`)
 })
