20c6294521ec822c021619697edbee7c1bf5f779
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2021-09-15 08:49:17 -0600

    resolving merge conflicts

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ const path = require('path')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-<<<<<<< HEAD
 
 app.get('/', (req, res) => {
   res.send({"Message": "Walkie Talkie Server Running..."})
 })
 
-=======
->>>>>>> 9c2379a117f4ee6206895a16b9f6b0763c3d05b6
 app.get('/history', (req, res) => {
     setTimeout(() => {
         res.send(mock_data)        
@@ -27,18 +24,12 @@ app.get('/recording', (req, res) => {
   opStream.pipe(res)
 })
 
-<<<<<<< HEAD
 // Not found error handler
 app.use((err, res, next) => {
   res.status(404).send({error: 'Not found!', status: 404})
 })
 
 // Generic server error
-=======
-app.use((err, res, next) => {
-  res.status(404).send({error: 'Not found!', status: 404})
-})
->>>>>>> 9c2379a117f4ee6206895a16b9f6b0763c3d05b6
 app.use((err, res, next) => {
   console.error('Error Handler:', err)
   const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
